6d7b43dec6c6dc700f1b01c8645685616c3bf6bd
alexkg <alex.k.goldsmith@gmail.com>
2017-04-18 09:46:56 +1000

    Simplifying project structure.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-function Global(F, e) {
-	try { return F('return this')() || (1, e)('this') }
-	catch (error) { return window }
-}
-
-const global = Global(Function, eval)
-
-export {global, global as GLOBAL}
-export default global
